7b2d76c4f9674ffddec474229b6078e8b9eb6a29
hannah <hannahnajwa@gmail.com>
2021-08-30 20:45:50 +0800

    set up react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,65 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React, { Fragment, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
+import './index.css';
 
 function App() {
-  return <Fragment></Fragment>;
+  const [isAuthenticated, setTisAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => setTisAuthenticated(boolean);
+
+  return (
+    <Fragment>
+      <Router>
+        <div className='container'>
+          <Switch>
+            <Route
+              exact
+              path='/login'
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to='/dashboard' />
+                )
+              }
+            />
+            <Route
+              exact
+              path='/register'
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to='/login' />
+                )
+              }
+            />
+            <Route
+              exact
+              path='/dashboard'
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to='/login' />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
